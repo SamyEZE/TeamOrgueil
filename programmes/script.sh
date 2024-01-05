674cae4362a37d98c0d03e6d3529cfc8f66c7c56
melissemhm <melissa.mahmoudi.09@gmail.com>
2024-01-05 21:33:25 +0100

    modifications des scripts

--- programmes/script.sh
+++ programmes/script.sh
@@ -2,23 +2,32 @@
 
 # Vérifie si l'argument du chemin du fichier est fourni
 if [[ $# -ne 1 ]]; then
-    echo "Usage : $0 chemin_du_fichier"
-    exit
+    echo "Usage : $0 fichier_des_urls"
+    exit 1
 fi
 
-URLS=$1
-MOT=$FICHIER
+# Préparation des chemins relatifs
+URLS_FILE="../URLs/$1"
+FICHIER=$(basename "$URLS_FILE" .txt)
+OUTPUT_DIR="../tableaux"
+ASPIRATION_DIR="../aspirations"
+DUMP_TEXTS_DIR="../dump-texts"
+CONTEXTE_DIR="../contextes"
+CONCORDANCES_DIR="../concordances"
 
-# Obtient le nom de base du fichier sans l'extension .txt et convertit en minuscules
-FICHIER=$(basename "$1" .txt | tr '[:upper:]' '[:lower:]')
+# Création des répertoires nécessaires s'ils n'existent pas
+mkdir -p "$OUTPUT_DIR" "$ASPIRATION_DIR" "$DUMP_TEXTS_DIR" "$CONTEXTE_DIR" "$CONCORDANCES_DIR"
 
-# Associe le nom du fichier à une langue
+# Association du nom du fichier à une langue
 case $FICHIER in
-    "orgueil")
-        LANGUE="fr"
+    "fierté")
+        LANGUE="fr1"  # Modifier ici pour "fr1"
         ;;
     "pride")
         LANGUE="en"
+        ;;
+    "orgueil")
+        LANGUE="fr2"  # Modifier ici pour "fr2"
         ;;
     "orgullo")
         LANGUE="es"
@@ -27,16 +36,17 @@ case $FICHIER in
         LANGUE="tr"
         ;;
     *)
-        echo "Langue non supportée"
+        echo "Langue non reconnue à partir du nom du fichier."
         exit 1
         ;;
 esac
 
-# Définition du fichier de sortie
-OUTPUT_FILE="../tableaux/tableau_${LANGUE}.html"
+# Nom du fichier de sortie
+OUTPUT_FILE="$OUTPUT_DIR/tableau_$LANGUE.html"
+
 
 # Création de l'en-tête du fichier HTML
-cat <<EOF > $OUTPUT_FILE
+cat <<EOF > "$OUTPUT_FILE"
 <!DOCTYPE html>
 <html lang="$LANGUE">
 <head>
@@ -62,7 +72,7 @@ cat <<EOF > $OUTPUT_FILE
         <div class="hero-body">
             <div class="container has-text-centered">
                 <h1 class="title hero-title">
-                    Tableau de $MOT
+                    Tableau de $FICHIER
                 </h1>
             </div>
         </div>
@@ -84,59 +94,70 @@ cat <<EOF > $OUTPUT_FILE
                         <th>Compte</th>
                     </tr>
                 </thead>
+                <tbody>
 EOF
 
 lineno=1
 
 # Boucle de lecture de chaque ligne du fichier d'URLs
-while read -r URL; do
-    FICHIER_ASPIRATION="../aspirations/${LANGUE}-${lineno}.html"
+while IFS= read -r URL; do
+    if [ -z "$URL" ]; then
+        # Si la ligne est vide, on passe à la suivante
+        continue
+    fi
+
+    FICHIER_ASPIRATION="$ASPIRATION_DIR/${LANGUE}-${lineno}.html"
+    FICHIER_DUMP="$DUMP_TEXTS_DIR/${LANGUE}-${lineno}.txt"
+    FICHIER_CONTEXTE="$CONTEXTE_DIR/${LANGUE}-${lineno}.txt"
+    CONCORDANCIER="$CONCORDANCES_DIR/${LANGUE}/${LANGUE}-${lineno}.html"
 
     response=$(curl -s -L -w "%{http_code}" -o "$FICHIER_ASPIRATION" "$URL")
-    encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null "$URL" | grep -o "charset=\S+" | cut -d"=" -f2 | tail -n 1 | tr '[:lower:]' '[:upper:]')
-    COMPTE=0
-    FICHIER_DUMP="NA"
-    FICHIER_CONTEXTE="NA"
-    CONCORDANCIER="NA"
 
-    if [ $response -eq 200 ]; then
-        # Création du dump texte
-        if [[ ! $encoding == "UTF-8" ]]; then
-            iconv -f "$encoding" -t "UTF-8" "$FICHIER_ASPIRATION" > "temp.html"
-            mv "temp.html" "$FICHIER_ASPIRATION"
-            encoding="UTF-8"
-        fi
-        FICHIER_DUMP="../dump-texts/${LANGUE}-${lineno}.txt"
-        lynx -assume_charset="UTF-8" -dump -nolist "$FICHIER_ASPIRATION" > "$FICHIER_DUMP"
-
-        COMPTE=$(grep -i -o "$MOT" $FICHIER_DUMP | wc -l)
-
-        FICHIER_CONTEXTE="../contextes/${LANGUE}-${lineno}.txt"
-        grep -i -C 3 "$MOT" $FICHIER_DUMP > $FICHIER_CONTEXTE
-
-        programmes/concordancier.sh $MOT $lineno $FICHIER_CONTEXTE $LANGUE
-        CONCORDANCIER="../concordances/${LANGUE}-${lineno}.html"
-        echo "<tbody>
-            <tr>
-                <th>$lineno</th><td>$URL</td><td><a href='$FICHIER_ASPIRATION'>Aspiration</a></td><td><a href='$FICHIER_DUMP'>Dump</a></td><td><a href='$FICHIER_CONTEXTE'>Contexte</a></td><td><a href='$CONCORDANCIER'>Concordancier</a></td><td>$response</td><td>$encoding</td><td>$COMPTE</td>
-            </tr>
-            </tbody>" >> $OUTPUT_FILE
-    else
-        FICHIER_ASPIRATION="NA"
-        echo "<tbody>
-            <tr>
-                <th>$lineno</th><td>$URL</td><td>$FICHIER_ASPIRATION</td><td>$FICHIER_DUMP</td><td>$FICHIER_CONTEXTE</td><td>$CONCORDANCIER</td><td>$response</td><td>$encoding</td><td>$COMPTE</td>
-            </tr>
-            </tbody>" >> $OUTPUT_FILE
+    if [ "$response" -ne 200 ]; then
+        echo "URL $lineno : Réponse HTTP non 200, URL ignorée."
+        lineno=$((lineno + 1))
+        continue
     fi
-    lineno=$(expr $lineno + 1)
-    echo "Traitement de l'URL $lineno terminé"
-done < "$URLS"
 
-echo "         </table>
+    # Analyse du fichier aspiration pour créer un dump
+    lynx -assume_charset="UTF-8" -dump -nolist "$FICHIER_ASPIRATION" > "$FICHIER_DUMP"
+
+    # Compte les occurrences du mot
+    COMPTE=$(grep -i -o "$FICHIER" "$FICHIER_DUMP" | wc -l)
+    if [ "$COMPTE" -eq 0 ]; then
+        echo "URL $lineno : Aucune occurrence de '$FICHIER', URL ignorée."
+        lineno=$((lineno + 1))
+        continue
+    fi
+
+    # Extraction du contexte autour des occurrences du mot
+    grep -i -C 3 "$FICHIER" "$FICHIER_DUMP" > "$FICHIER_CONTEXTE"
+
+    # Appel au script concordancier
+    ./concordancier.sh "$LANGUE" "$FICHIER" "$FICHIER_CONTEXTE"
+
+    # Ajout des informations dans le fichier HTML
+    echo "<tr>" >> "$OUTPUT_FILE"
+    echo "<th>$lineno</th>" >> "$OUTPUT_FILE"
+    echo "<td><a href='$URL'>$URL</a></td>" >> "$OUTPUT_FILE"
+    echo "<td><a href='$FICHIER_ASPIRATION'>Aspiration</a></td>" >> "$OUTPUT_FILE"
+    echo "<td><a href='$FICHIER_DUMP'>Dump</a></td>" >> "$OUTPUT_FILE"
+    echo "<td><a href='$FICHIER_CONTEXTE'>Contexte</a></td>" >> "$OUTPUT_FILE"
+    echo "<td><a href='$CONCORDANCIER' target='_blank'>Concordancier</a></td>" >> "$OUTPUT_FILE"
+    echo "<td>$response</td>" >> "$OUTPUT_FILE"
+    echo "<td>UTF-8</td>" >> "$OUTPUT_FILE"
+    echo "<td>$COMPTE</td>" >> "$OUTPUT_FILE"
+    echo "</tr>" >> "$OUTPUT_FILE"
+
+    lineno=$((lineno + 1))
+done < "$URLS_FILE"
+
+# Fermeture des balises HTML
+echo "                </tbody>
+            </table>
         </div>
+    </section>
 </body>
-</html>" >> $OUTPUT_FILE
+</html>" >> "$OUTPUT_FILE"
 
-echo "gg wp"
-
+echo "Traitement terminé"
